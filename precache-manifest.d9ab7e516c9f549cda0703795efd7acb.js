a821e14aafb63759eceb8c113802cc676f14a059
Pratima Gautam <astute.yard@gmail.com>
2020-04-04 15:24:02 +0545

    Updates

---
+++ precache-manifest.d9ab7e516c9f549cda0703795efd7acb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6392357afb5100b4318c0c7df9e0645",
+    "url": "/react-notes-app/index.html"
+  },
+  {
+    "revision": "cffb2e6154c8c4688f56",
+    "url": "/react-notes-app/static/css/main.090b6109.chunk.css"
+  },
+  {
+    "revision": "c6d64fa629cca5c9224d",
+    "url": "/react-notes-app/static/js/2.6e12bced.chunk.js"
+  },
+  {
+    "revision": "8e2b8a31d44c99cb09a3393db51211a7",
+    "url": "/react-notes-app/static/js/2.6e12bced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cffb2e6154c8c4688f56",
+    "url": "/react-notes-app/static/js/main.33c9e064.chunk.js"
+  },
+  {
+    "revision": "a4546f6abddeed06fac1",
+    "url": "/react-notes-app/static/js/runtime-main.8b04c739.js"
+  }
+]);
