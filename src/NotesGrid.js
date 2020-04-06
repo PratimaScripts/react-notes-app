fd95c7ff114f00b85d684bdf62db15c02afcca3c
Pratima Gautam <astute.yard@gmail.com>
2020-04-06 08:18:57 +0545

    Created ref using React.createRef()

--- src/NotesGrid.js
+++ src/NotesGrid.js
@@ -1,43 +1,47 @@
 import Masonry from "masonry-layout";
 
-import React from 'react';
+import React from "react";
 import Note from "./Note";
 export default class NotesGrid extends React.Component {
-    componentDidMount() {
-      var grid = this.refs.grid;
-      this.msnry = new Masonry(grid, {
-        itemSelector: ".note",
-        columnWidth: 200,
-        gutter: 10,
-        isFitWidth: true
-      });
-    }
-  
-    componentDidUpdate(prevProps) {
-      if (this.props.notes.length !== prevProps.notes.length) {
-        this.msnry.reloadItems();
-        this.msnry.layout();
-      }
-    }
-  
-    render() {
-      var onNoteDelete = this.props.onNoteDelete;
-  
-      return (
-        <div className="notes-grid" ref="grid">
-          {this.props.notes.map(function(note) {
-            return (
-              <Note
-                key={note.id}
-                onDelete={onNoteDelete.bind(null, note)}
-                color={note.color}
-              >
-                {note.text}
-              </Note>
-            );
-          })}
-        </div>
-      );
+  constructor(props) {
+    super(props);
+    this.gridRef = React.createRef();
+  }
+
+  componentDidMount() {
+    // var grid = this.refs.grid;
+    this.msnry = new Masonry(this.gridRef.current, {
+      itemSelector: ".note",
+      columnWidth: 200,
+      gutter: 10,
+      isFitWidth: true,
+    });
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.notes.length !== prevProps.notes.length) {
+      this.msnry.reloadItems();
+      this.msnry.layout();
     }
   }
-  
+
+  render() {
+    var onNoteDelete = this.props.onNoteDelete;
+
+    return (
+      <div className="notes-grid" ref={this.gridRef}>
+        {this.props.notes.map(function (note) {
+          return (
+            <Note
+              key={note.id}
+              onDelete={onNoteDelete.bind(null, note)}
+              color={note.color}
+            >
+              {note.text}
+            </Note>
+          );
+        })}
+      </div>
+    );
+  }
+}
